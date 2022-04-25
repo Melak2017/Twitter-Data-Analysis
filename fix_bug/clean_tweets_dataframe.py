4e8531ee854c4986fe38106f97b4dbd1b1dd7d08
Melak2017 <melakuandarge615@gmail.com>
2022-04-25 23:16:27 +0300

    add script on clean_tweet_dataframe

--- fix_bug/clean_tweets_dataframe.py
+++ fix_bug/clean_tweets_dataframe.py
@@ -34,3 +34,42 @@ class CleanTweets:
         self.df = self.df.drop_duplicates().drop_duplicates(subset='original_text')
 
         return df
+
+    def convert_to_datetime(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        convert column to datetime
+        """
+        self.df['created_at'] = pd.to_datetime(
+            self.df['created_at'], errors='coerce')
+
+        self.df = self.df[self.df['created_at'] >= '2020-12-31']
+
+        return self.df
+
+    def convert_to_numbers(self) -> pd.DataFrame:
+        """
+        convert columns like polarity, subjectivity, retweet_count
+        favorite_count etc to numbers
+        """
+        self.df['polarity'] = pd.to_numeric(
+            self.df['polarity'], errors='coerce')
+        self.df['retweet_count'] = pd.to_numeric(
+            self.df['retweet_count'], errors='coerce')
+        self.df['favorite_count'] = pd.to_numeric(
+            self.df['favorite_count'], errors='coerce')
+
+        return self.df
+
+    def remove_non_english_tweets(self) -> pd.DataFrame:
+        """
+        remove non english tweets from lang
+        """
+
+        self.df = self.df.query("lang == 'en' ")
+
+        return self.df
+
+
+if __name__ == "__main__":
+    tweet_df = pd.read_csv("processed_tweet_data.csv")
+    cleaner = CleanTweets(tweet_df)
