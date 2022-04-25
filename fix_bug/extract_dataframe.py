2368fb9a5e438904c54fa7433017ea24f480ab52
Melak2017 <melakuandarge615@gmail.com>
2022-04-25 20:13:58 +0300

    added the code retweet_count func

--- fix_bug/extract_dataframe.py
+++ fix_bug/extract_dataframe.py
@@ -120,6 +120,18 @@ class TweetDfExtractor:
 
         return favorite_count
 
+    def find_retweet_count(self) -> list:
+
+        retweet_count = []
+        for tweet in self.tweets_list:
+            if 'retweeted_status' in tweet.keys():
+                retweet_count.append(
+                    tweet['retweeted_status']['retweet_count'])
+            else:
+                retweet_count.append(0)
+
+        return retweet_count
+
 
 if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
