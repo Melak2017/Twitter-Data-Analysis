4e246de21cef83dda7da3072458d2124d8876973
Melak2017 <melakuandarge615@gmail.com>
2022-04-29 00:50:56 +0300

    fix workflow error

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -5,8 +5,8 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join('fix_bug')))
 
-from extract_dataframe import read_json
 from extract_dataframe import TweetDfExtractor
+from extract_dataframe import read_json
 #
 _, tweet_list = read_json("data/Economic_Twitter_Data.json")
 
